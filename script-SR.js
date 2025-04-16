5489c278d115bb40a4924de5350116e94b720a69
cnmathers <reevescandace@ymail.com>
2025-04-16 17:13:20 -0500

    Add files via upload

---
+++ script-SR.js
@@ -0,0 +1,5 @@
+document.getElementById("clickBtn").addEventListener("click", function() {
+    const message = document.getElementById("message");
+    message.textContent = "You clicked the button! 🎉";
+  });
+  
